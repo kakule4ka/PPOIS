edc51570b7b3fd9049459b8afbe076af5428f31d
Артём Перфоратор <artemmarudin1@gmail.com>
2025-10-24 20:53:27 +0300

    Clean up vector.h by removing comments and whitespace
    Removed unnecessary comments and whitespace in vector.h.

--- lab1/vector/vector.h
+++ lab1/vector/vector.h
@@ -14,38 +14,34 @@ public:
     vector(double x, double y, double z);
     vector(const vector& other) = default;
     vector(std::initializer_list<double> list);
-    
+
     void swap(vector& other) noexcept;
     double size() const;
-    
+
     vector& operator=(const vector& other) = default;
     vector& operator=(std::initializer_list<double> list);
-    
-    // Арифметические операторы
+
     vector operator+(const vector& vec) const;
     vector operator-(const vector& vec) const;
     vector& operator+=(const vector& vec);
     vector& operator-=(const vector& vec);
-    
-    // Умножение и деление на скаляр
+
     vector operator*(double value) const;
+    friend vector operator*(double value, const vector& vec);
     vector& operator*=(double value);
+
     vector operator/(double value) const;
     vector& operator/=(double value);
-    
-    // Скалярное и векторное произведения
+
     double operator*(const vector& vec) const;
     vector operator%(const vector& vec) const;
     vector& operator%=(const vector& vec);
-    
-    // Поэлементное деление
+
     vector operator/(const vector& vec) const;
     vector& operator/=(const vector& vec);
-    
-    // Косинус угла между векторами
+
     double operator^(const vector& vec) const;
-    
-    // Операторы сравнения
+
     bool operator==(const vector& vec) const;
     bool operator!=(const vector& vec) const;
     bool operator>(const vector& vec) const;
@@ -53,7 +49,6 @@ public:
     bool operator<(const vector& vec) const;
     bool operator<=(const vector& vec) const;
 
-    // Операторы ввода-вывода
     friend std::ostream& operator<<(std::ostream& os, const vector& vec);
     friend std::istream& operator>>(std::istream& is, vector& vec);
 };
